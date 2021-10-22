eba9329724b59504020b6fc538e1ef634e369b74
Vladimir Kolchin <Kolchin_Vladimir@mail.ru>
2021-10-22 12:37:34 +0300

    add win2.py

--- bginfo/bginfo.py
+++ bginfo/bginfo.py
@@ -6,8 +6,11 @@ from bginfo import Ui_MainWindow
 
 
 class MainWindow(QMainWindow):
+    PATH_CONFIG = sys.path[0] + "/resources/config/"
+
     def __init__(self):
         super().__init__()
+        self.path_config = MainWindow.PATH_CONFIG
         self.ui_main = Ui_MainWindow()
         self.ui_main.setupUi(self)
         self.show()
@@ -20,6 +23,7 @@ class MainWindow(QMainWindow):
 
     def act_install(self):
         print("Нажата кнопка установить")
+        print("Каталог с конфигами:" + self.path_config)
 
     def act_uninstall(self):
         print("Нажата кнопка удалить")
