5d0501d7ac0c5e78c1d04ded256abb4d252cd3fe
Vladimir Kolchin <Kolchin_Vladimir@mail.ru>
2021-11-01 20:40:55 +0300

    add func depency_checking

--- ubuntu/ubuntu.py
+++ ubuntu/ubuntu.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import pkg_resources
 import subprocess
 import sys
+import threading
 from ubuntu import Ui_Form
 from PyQt5.QtWidgets import QApplication, QDialog, QMessageBox
 
@@ -16,7 +18,7 @@ class MainWindow(QDialog):
         self.act_button()
 
     def list_program(self):
-        self.list = ["pycharm-community", "qtdesigner"]
+        self.list = ["pycharm-community", "qtdesigner", "timeshift"]
         for x in self.list:
             self.ui_main.comboBox.addItem(x)
 
@@ -47,20 +49,27 @@ class InstallProgram(object):
     def __init__(self, program=None):
         super().__init__()
         self.program = program
+        self.command = None
         self.install()
 
     def install(self):
         print("Установка программы {}".format(self.program))
         if self.program == "pycharm-community":
-            action_program(command="sudo snap install pycharm-community --classic")
+            self.command = "sudo snap install pycharm-community --classic"
         if self.program == "qtdesigner":
-            action_program(command="sudo apt-get install python3-pyqt5 qtcreator "
-                                   "pyqt5-dev-tools qttools5-dev-tools -y")
+            self.command = "sudo apt-get install python3-pyqt5 qtcreator pyqt5-dev-tools qttools5-dev-tools -y"
+        if self.program == "timeshift":
+            self.command = "sudo apt-get install timeshift -y"
+        t = threading.Thread(target=action_program, name="install_timeshift",
+                             args=(self.command,))
+        t.start()
+        t.join()
 
 
 class UninstallProgram(object):
     def __init__(self, program=None):
         super().__init__()
+        self.command = None
         self.program = program
         self.uninstall()
 
@@ -69,22 +78,46 @@ class UninstallProgram(object):
         if self.program == "pycharm-community":
             print("Не поддерживается удаление!")
         if self.program == "qtdesigner":
-            action_program(command="sudo apt-get remove qtcreator "
-                                   "pyqt5-dev-tools qttools5-dev-tools -y")
+            self.command = "sudo apt-get remove qtcreator pyqt5-dev-tools qttools5-dev-tools -y"
+        if self.program == "timeshift":
+            self.command = "sudo apt-get remove timeshift -y"
+        if self.command:
+            t = threading.Thread(target=action_program, name="install_timeshift",
+                                 args=(self.command,))
+            t.start()
+            t.join()
 
 
 def action_program(command):
+    st = True
     if command:
         proc = subprocess.Popen(command, shell=True,
                                 stdout=subprocess.PIPE,
                                 stderr=subprocess.PIPE)
-        out, err = proc.communicate()
-        if out.decode("utf-8"): print(out.decode("utf-8"))
-        if err.decode('utf-8'): print(err.decode('utf-8'))
+        while st:
+            st = proc.stdout.readline()
+            print(st.decode("utf-8"), end="")
+        # out, err = proc.communicate()
+        # if out.decode("utf-8"): print(out.decode("utf-8"))
+        # if err.decode('utf-8'): print(err.decode('utf-8'))
+
+
+# Проверка зависимостей для запуска программы
+def dependency_checking():
+    print("Запуск функции проверки установленных зависимостей!")
+    state_pack = False
+    for d in pkg_resources.working_set:
+        if str(d).split()[0] == "PyQt5":
+            print("Пакет PyQt5 уже установлен в системе!")
+            state_pack = True
+    if not state_pack:
+        print("Устанавливаем необходимые зависимости!")
+        action_program(command="sudo apt-get install python3-pyqt5 -y")
 
 
 def main():
     print("Запуск функции main")
+    dependency_checking()
     app = QApplication(sys.argv)
     mw = MainWindow()
     mw.show()
