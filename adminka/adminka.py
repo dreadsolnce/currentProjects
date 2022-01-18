3278bf81ff38d5b9dd658191cce251ed354ce136
Vladimir Kolchin <Kolchin_Vladimir@mail.ru>
2022-01-18 10:25:02 +0300

    add new menu

--- adminka/adminka.py
+++ adminka/adminka.py
@@ -6,8 +6,7 @@ import subprocess
 import sys
 import threading
 
-from PyQt5.Qt import Qt
-from PyQt5 import QtWidgets, QtGui, QtCore
+from PyQt5 import QtWidgets, QtCore, QtGui
 from PyQt5.QtWidgets import QMessageBox
 from PyQt5.QtWidgets import QDesktopWidget
 
@@ -24,122 +23,51 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def __init__(self):
         super().__init__()
-        # Инициализация переменных
-        self.p = None   # Объект класса программ
-        self.os_ver = resources.OsVersion()  # Версия ОС
-        # Инициализация основного окна
         self.gui = resources.Ui_MainWindow()
-        self.initializationMainWindow()
+        self.main_settings = resources.Ui_MainSettingsWindow()
+        self.remote_settings = resources.Ui_RemoteSettingsWindow()
 
-    def initializationMainWindow(self):
-        print("Инициализация основного окна программы")
-        self.gui.setupUi(self)
-        # Задание параметров основного окна
-        self.gui.checkBox.setVisible(False)
-        self.gui.pushButtonInstall.setVisible(False)
-        self.gui.pushButtonRemove.setVisible(False)
+        self.width = 809
+        self.height = 441
+
+        self.mainWin()
+
+    def mainWin(self):
+        self.gui.setupUi(self, self.width, self.height)
+
         # Создание иконки программы
         icon = QtGui.QIcon()
         icon.addPixmap(QtGui.QPixmap(logo))
         self.setWindowIcon(icon)
-        # Задание параметров treeWidget
-        self.gui.treeWidget.setVisible(False)
-        self.gui.treeWidget.setFocusPolicy(Qt.NoFocus)
-        self.gui.treeWidget.setIndentation(2)
-        self.gui.treeWidget.setColumnWidth(0, 60)
-        self.gui.treeWidget.setColumnWidth(1, 250)
-        # Изменение статус бара
-        self.gui.statusbar.showMessage("Основное окно программы")
-        self.gui.centralwidget.setStatusTip("Основное окно программы")
-        self.gui.InstallRemove.setStatusTip("Установка и удаление программ")
-        self.gui.treeWidget.setStatusTip("Список программ для установки и удаления")
-        self.gui.checkBox.setStatusTip("Выделить всё")
-        self.gui.pushButtonInstall.setStatusTip("Установка отмеченных программ")
-        self.gui.pushButtonRemove.setStatusTip("Удаление отмеченных программ")
-        self.gui.action_Exit.setStatusTip("Выход из программы")
-        # Центрирование окна
-        self.winCenter()
-        self.show()
-        self.action()
 
-    def action(self):
-        self.gui.InstallRemove.triggered.connect(self.clkInstallRemove)
-        self.gui.checkBox.clicked.connect(self.clkCheckbox)
-        self.gui.pushButtonInstall.clicked.connect(lambda: self.clkPushButtonInstall(action="install"))
-        self.gui.pushButtonRemove.clicked.connect(lambda: self.clkPushButtonInstall(action="remove"))
+        self.show()
+        self.winCenter()
+        self.actionMainWindow()
+
+    def actionMainWindow(self):
+        self.gui.action_MainSettings.triggered.connect(self.MenuMainSettingsWindows)
+        self.gui.action_OpenRemoteSettings.triggered.connect(self.MenuRemoteSettingsWindows)
         self.gui.action_Exit.triggered.connect(lambda: sys.exit())
 
-    # В зависимости от версии ОС формируем список программ
-    def clkInstallRemove(self):
-        print("Нажата кнопка Установка/Удаление")
-        self.p = resources.Programs(self.os_ver)  # Инициализируем класс программы
-        self.enableWidget()
-        lst_prog = self.p.list_program   # Список программ
-        if not lst_prog:
-            QMessageBox.critical(self, "Ошибка!", "Не поддерживаемая версия ОС", QMessageBox.Ok)
-        else:
-            self.fillingProgramList(lst_prog)
+    def MenuMainSettingsWindows(self):
+        print("Выбрано меню Локальная настройка > Основные параметры")
+        self.main_settings.setupUi(self, self.width, self.height)
+        self.main_settings.frame_1.setEnabled(False)
+        self.actionMenuMainSettingsWindows()
 
-    # Формируем словарь с именем программы и ключом (состояние программы)
-    def fillingProgramList(self, lst_program):
-        self.gui.treeWidget.clear()
-        state_program = self.p.stateProg()  # Словарь: программа:статус программы (0 - не установлена, 1 - установлена)
-        self.fillingGadget(state_program, lst_program)
+    def actionMenuMainSettingsWindows(self):
+        self.main_settings.pushButton_insprog.clicked.connect(lambda: print("Нажата кнопка установить"))
+        self.main_settings.action_OpenRemoteSettings.triggered.connect(self.MenuRemoteSettingsWindows)
+        self.main_settings.action_Exit.triggered.connect(lambda: sys.exit())
 
-    # Заполняем гаджет со списком программ
-    def fillingGadget(self, state_program, lst_program):
-        for name in lst_program:
-            child = QtWidgets.QTreeWidgetItem(self.gui.treeWidget)
-            child.setCheckState(0, Qt.Unchecked)
-            child.setText(1, name)
-            if state_program[name] == 0:
-                child.setForeground(2, QtGui.QBrush(Qt.darkGreen))
-                child.setText(2, "Установлена")
-            elif state_program[name] != 0:
-                child.setForeground(2, QtGui.QBrush(Qt.darkRed))
-                child.setText(2, "Отсутствует")
+    def MenuRemoteSettingsWindows(self):
+        print("Выбрано меню Удалённая настройка > Открыть")
+        self.remote_settings.setupUi(self, self.width, self.height)
+        self.actionMenuRemoteSettingsWindows()
 
-    def clkCheckbox(self):
-        print("Нажат чекбокс")
-        if self.gui.checkBox.isChecked():
-            self.allCheck(state=True)
-            print("Выделяем все чек боксы")
-        elif not self.gui.checkBox.isChecked():
-            self.allCheck(state=False)
-            print("Снимаем выделение со всех чек боксов")
-
-    def allCheck(self, state):
-        count_row = self.gui.treeWidget.topLevelItemCount()
-        print("Количество строк в таблице: {}".format(count_row))
-        for count in range(count_row):
-            current_element = self.gui.treeWidget.topLevelItem(count)
-            if state:
-                current_element.setCheckState(0, Qt.Checked)
-            elif not state:
-                current_element.setCheckState(0, Qt.Unchecked)
-
-    def clkPushButtonInstall(self, action=None):
-        if action == "install":
-            print("Нажата кнопка установить")
-        elif action == "remove":
-            print("Нажата кнопка удалить")
-        count_row = self.gui.treeWidget.topLevelItemCount()
-        name_prog_list = []
-        for count in range(count_row):
-            current_element = self.gui.treeWidget.topLevelItem(count)
-            if current_element.checkState(0):
-                name_prog = current_element.text(1)
-                name_prog_list.append(name_prog)
-        if name_prog_list:
-            self.p.actionProg(os_ver=self.os_ver, action=action, lst_name_prog=name_prog_list)
-            self.clkInstallRemove()  # # Обновляем список состояния программ
-
-    # Показ виджетов
-    def enableWidget(self):
-        self.gui.treeWidget.setVisible(True)
-        self.gui.checkBox.setVisible(True)
-        self.gui.pushButtonInstall.setVisible(True)
-        self.gui.pushButtonRemove.setVisible(True)
+    def actionMenuRemoteSettingsWindows(self):
+        self.remote_settings.action_MainSettings.triggered.connect(self.MenuMainSettingsWindows)
+        self.remote_settings.action_Exit.triggered.connect(lambda: sys.exit())
 
     # Центрирование окна относительно экрана
     def winCenter(self):
@@ -147,6 +75,11 @@ class MainWindow(QtWidgets.QMainWindow):
         qp = QDesktopWidget().availableGeometry().center()
         qr.moveCenter(qp)
         self.move(qr.topLeft())
+
+    # Событие при изменении размера окна
+    def resizeEvent(self, event):
+        self.width = self.size().width()
+        self.height = self.size().height()
 
 
 # Окно таймера
